c454a3fe8d774f8b2e0831688f0c4857797bcbda
Dick Kreisberg <rkreisberg@systemsbiology.org>
2012-06-12 18:52:43 -0700

    clean up demo a little more

--- test/circvis/js/circvis_configure.js
+++ test/circvis/js/circvis_configure.js
@@ -3,7 +3,6 @@
     var width=1200,
         height=1200,
         cnv_ring_height = 60,
-        tick_keys = ['METH','CNVR','hsa-miR','GEXP','GNAB'],
         color_scale =    { 'GEXP': '#1f77b4',
             //blue
             'METH': '#2ca02c',
@@ -83,157 +82,157 @@
 
     _.each(links, function(item){hovercard_links_config[item.label]=item;});
     var data = function(div) { return {
-           PLOT: {
-               container: div,
-               width : width,
-               height: height,
-               vertical_padding : 10,
-               horizontal_padding: 10,
-               enable_pan : false,
-               enable_zoom : false,
-               show_legend: true,
-               legend_corner : 'ne',
-               legend_radius  : 45
-           },
+        PLOT: {
+            container: div,
+            width : width,
+            height: height,
+            vertical_padding : 10,
+            horizontal_padding: 10,
+            enable_pan : false,
+            enable_zoom : false,
+            show_legend: true,
+            legend_corner : 'ne',
+            legend_radius  : 45
+        },
 
-           GENOME: {
-               DATA:{
-                   key_order : chrom_keys,
-                   key_length :_.map(chrom_keys, function(key) {return {chr_name:key, chr_length:chrom_attr[key].length};})
-               },
-               OPTIONS: {
-                   radial_grid_line_width : 2,
-                   label_layout_style:'clock',
-                   label_font_style:'16px helvetica'
-               }
-           },
+        GENOME: {
+            DATA:{
+                key_order : chrom_keys,
+                key_length :_.map(chrom_keys, function(key) {return {chr_name:key, chr_length:chrom_attr[key].length};})
+            },
+            OPTIONS: {
+                radial_grid_line_width : 2,
+                label_layout_style:'clock',
+                label_font_style:'16px helvetica'
+            }
+        },
 
-           WEDGE:[
-               {
-                   PLOT : {
-                       height : 35,
-                       type : 'karyotype'
-                   } ,
-                   DATA:{
-                       data_array : cytoband
-                   },
-                   OPTIONS: {
+        WEDGE:[
+            {
+                PLOT : {
+                    height : 35,
+                    type : 'karyotype'
+                } ,
+                DATA:{
+                    data_array : cytoband
+                },
+                OPTIONS: {
 
-                       legend_label : 'Cytogenic Bands',
-                       legend_description : 'Cytogenic Bands',
-                       listener : function() {return null;},
-                       outer_padding: 15,
-                       stroke_style:'#777',
-                       line_width:'0.5px',
-                       tooltip_items:{'Cytogenic Band':'label',
-                           "Location": function(feature) { return 'Chr ' + feature.chr + ' ' + feature.start + (feature.end ? '-' + feature.end : '');}
-                       }
-                   }
-               },
-               {   PLOT : {
-                   height : cnv_ring_height,
-                   type : 'glyph'
-               } ,
-                   DATA:{
-                       data_array : [],//cnv
-                       value_key:'clin_alias'
-                   },
-                   OPTIONS: {
-                       fill_style:'orange',
-                       stroke_style:'white',
-                       line_width:1,
-                       legend_label : 'Clinical Associations',
-                       shape:'dot',
-                       radius:4,
-                       legend_description : 'Clinical Associations',
-                       listener : function() {return null;},
-                       outer_padding: 15,
-                       tooltip_items: hovercard_items_config,
-                       tooltip_links: hovercard_links_config
-                   }
-               },
-               {
-                   PLOT : {
+                    legend_label : 'Cytogenic Bands',
+                    legend_description : 'Cytogenic Bands',
+                    listener : function() {return null;},
+                    outer_padding: 15,
+                    stroke_style:'#777',
+                    line_width:'0.5px',
+                    tooltip_items:{'Cytogenic Band':'label',
+                        "Location": function(feature) { return 'Chr ' + feature.chr + ' ' + feature.start + (feature.end ? '-' + feature.end : '');}
+                    }
+                }
+            },
+            {   PLOT : {
+                height : cnv_ring_height,
+                type : 'glyph'
+            } ,
+                DATA:{
+                    data_array : [],//cnv
+                    value_key:'clin_alias'
+                },
+                OPTIONS: {
+                    fill_style:'orange',
+                    stroke_style:'white',
+                    line_width:1,
+                    legend_label : 'Clinical Associations',
+                    shape:'dot',
+                    radius:4,
+                    legend_description : 'Clinical Associations',
+                    listener : function() {return null;},
+                    outer_padding: 15,
+                    tooltip_items: hovercard_items_config,
+                    tooltip_links: hovercard_links_config
+                }
+            },
+            {
+                PLOT : {
 
-                       height : 60,
-                       type : 'scatterplot'
-                   },
-                   DATA:{
-                       data_array : [],//link_density,
-                       value_key : 'mutation_count'
-                   },
-                   OPTIONS: {
-                       legend_label : 'Mutation Count',
-                       legend_description : 'Mutation Count',
-                       min_value : 0,
-                       max_value : 6,
-                       base_value : 0,
-                       radius : 6,
-                       shape : 'circle',
-                       outer_padding: 15,
-                       stroke_style : null,
-                       tooltip_items: hovercard_items_config,
-                       tooltip_links: hovercard_links_config,
-                       fill_style: tick_colors,
-                       listener : function() {return null;}
-                   }
-               }
-           ],
+                    height : 60,
+                    type : 'scatterplot'
+                },
+                DATA:{
+                    data_array : [],//link_density,
+                    value_key : 'mutation_count'
+                },
+                OPTIONS: {
+                    legend_label : 'Mutation Count',
+                    legend_description : 'Mutation Count',
+                    min_value : 0,
+                    max_value : 6,
+                    base_value : 0,
+                    radius : 6,
+                    shape : 'circle',
+                    outer_padding: 15,
+                    stroke_style : null,
+                    tooltip_items: hovercard_items_config,
+                    tooltip_links: hovercard_links_config,
+                    fill_style: tick_colors,
+                    listener : function() {return null;}
+                }
+            }
+        ],
 
-           TICKS : {
-               DATA: {
-                   data_array:[]
-               },
-               OPTIONS : {
-                   wedge_height: 15,
-                   wedge_width:0.7,
-                   overlap_distance:10000000, //tile ticks at specified base pair distance
-                   height : 120,
-                   display_legend : true,
-                   legend_corner : 'nw',
-                   fill_style : tick_colors,
-                   tooltip_items: hovercard_items_config,
-                   tooltip_links: hovercard_links_config
-               }
-           },
+        TICKS : {
+            DATA: {
+                data_array:[]
+            },
+            OPTIONS : {
+                wedge_height: 15,
+                wedge_width:0.7,
+                overlap_distance:10000000, //tile ticks at specified base pair distance
+                height : 120,
+                display_legend : true,
+                legend_corner : 'nw',
+                fill_style : tick_colors,
+                tooltip_items: hovercard_items_config,
+                tooltip_links: hovercard_links_config
+            }
+        },
 
-           NETWORK:{
-               DATA:{
-                   data_array : []//
-               },
-               OPTIONS: {
-                   outer_padding : 10,
-                   node_radius:6,
-                   node_fill_style : tick_colors,
-                   link_stroke_style : 'red',
-                   link_line_width:8,
-                   link_alpha : 0.6,
-                   node_highlight_mode : 'isolate',
-                   node_key : function(node) { return node.label;},
-                   node_tooltip_items :  hovercard_items_config,
-                   node_tooltip_links: hovercard_links_config,
-                   link_tooltip_items :  {
-                       'Target' : function(link) { var label = link.source.label.split(':'); return '<span style="color:'+tick_colors(link.source)+'">' +
-                               label_map[label[1]] + '</span> ' + label[2];},
-                       'Target Location' : function(link) { return 'Chr ' + link.source.chr + ' ' + link.source.start +
-                               (link.source.end ? '-' + link.source.end : '');},
-                       'Predictor' : function(link) { var label = link.target.label.split(':'); return '<span style="color:'+tick_colors(link.target)+'">' +
-                               label_map[label[1]] + '</span> ' + label[2];},
-                       'Predictor Location' : function(link) { return 'Chr ' + link.target.chr + ' ' + link.target.start +
-                               (link.target.end ? '-' + link.target.end : '');},
-                       Importance:'assoc_value1'
-                   }
-               }
-           }
-       };
+        NETWORK:{
+            DATA:{
+                data_array : []//
+            },
+            OPTIONS: {
+                outer_padding : 10,
+                node_radius:6,
+                node_fill_style : tick_colors,
+                link_stroke_style : 'red',
+                link_line_width:8,
+                link_alpha : 0.6,
+                node_highlight_mode : 'isolate',
+                node_key : function(node) { return node.label;},
+                node_tooltip_items :  hovercard_items_config,
+                node_tooltip_links: hovercard_links_config,
+                link_tooltip_items :  {
+                    'Target' : function(link) { var label = link.source.label.split(':'); return '<span style="color:'+tick_colors(link.source)+'">' +
+                        label_map[label[1]] + '</span> ' + label[2];},
+                    'Target Location' : function(link) { return 'Chr ' + link.source.chr + ' ' + link.source.start +
+                        (link.source.end ? '-' + link.source.end : '');},
+                    'Predictor' : function(link) { var label = link.target.label.split(':'); return '<span style="color:'+tick_colors(link.target)+'">' +
+                        label_map[label[1]] + '</span> ' + label[2];},
+                    'Predictor Location' : function(link) { return 'Chr ' + link.target.chr + ' ' + link.target.start +
+                        (link.target.end ? '-' + link.target.end : '');},
+                    Importance:'assoc_value1'
+                }
+            }
+        }
+    };
     };
 
     circvis = {};
     circvis.plot = function(div) {
         var circle_vis = new vq.CircVis();
-          var dataObject ={DATATYPE : "vq.models.CircVisData", CONTENTS : data(div) };
-          circle_vis.draw(dataObject);
-          return circle_vis;
-      }
+        var dataObject ={DATATYPE : "vq.models.CircVisData", CONTENTS : data(div) };
+        circle_vis.draw(dataObject);
+        return circle_vis;
+    }
 
 })();
