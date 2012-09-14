04f998d09008b3c45c2b419eff8c416f444d4e79
kleinone <kleinone@systemsbiology.org>
2012-09-13 19:03:42 -0700

    Fixed visualization element to be set by the jQuery plugin. Uncluttered the $.fn namespace.

--- src/scatterplot/scatterplot.jquery.js
+++ src/scatterplot/scatterplot.jquery.js
@@ -1,39 +1,52 @@
 !function($) {
-    $.fn.scatterplot = function(options) {
-        return this.each(function() {
-            var $this = $(this);
-            var vis = $(this).data("visquick.d3.scatterplot");
-            if (!vis) {
-                $this.data("visquick.d3.scatterplot", (vis = new vq.ScatterPlot()));
-                vis.draw(options);
-            }
-        });
+    var methods = {
+      init : function( options ) {
+          return this.each(function() {
+              var $this = $(this);
+              var vis = $(this).data("visquick.d3.scatterplot");
+              if (!vis) {
+                  options.CONTENTS.PLOT.container = $this.get(0);
+                  $this.data("visquick.d3.scatterplot", (vis = new vq.ScatterPlot()));
+                  vis.draw(options);
+              }
+          });
+      },
+      reset_data : function(data_array) {
+          return this.each(function() {
+              var vis = $(this).data("visquick.d3.scatterplot");
+              if (vis) {
+                  vis.resetData(data_array);
+              }
+          });
+      },
+      enable_zoom : function() {
+          return this.each(function() {
+              var vis = $(this).data("visquick.d3.scatterplot");
+              if (vis) {
+                  vis.enableZoom();
+              }
+          });
+      },
+      enable_brush : function() {
+          return this.each(function() {
+              var vis = $(this).data("visquick.d3.scatterplot");
+              if (vis) {
+                  vis.enableBrush();
+              }
+          });
+      }
     };
 
-    $.fn.reset_data = function(data_array) {
-        return this.each(function() {
-            var vis = $(this).data("visquick.d3.scatterplot");
-            if (vis) {
-                vis.resetData(data_array);
-            }
-        });
-    };
-
-    $.fn.enable_zoom = function() {
-        return this.each(function() {
-            var vis = $(this).data("visquick.d3.scatterplot");
-            if (vis) {
-                vis.enableZoom();
-            }
-        });
-    };
-
-    $.fn.enable_brush = function() {
-        return this.each(function() {
-            var vis = $(this).data("visquick.d3.scatterplot");
-            if (vis) {
-                vis.enableBrush();
-            }
-        });
+    $.fn.scatterplot = function( method ) {
+      // Method calling logic
+      if ( methods[method] ){
+          return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+      }
+      else if ( typeof method === 'object' || ! method ) {
+          return methods.init.apply( this, arguments );
+      }
+      else {
+          $.error( 'Method ' +  method + ' does not exist on jQuery.scatterplot' );
+      }
     };
 }(window.jQuery);
